df76cbcae7938d36ce9cd5a2dd38a5ef58b4262f
che30 <che.blancoo58@gmail.com>
2020-11-03 18:10:16 +0100

    improved Bubble_sort code

--- main.rb
+++ main.rb
@@ -1,15 +1,15 @@
 def Bubble_sort(arr)
     arr.each_index do|i|
         arr.each_index do |j|
-            if((j>i)&&(j<arr.length))
-                arr[i],arr[j]=arr[j],arr[i] if ((arr[j]<=>arr[i])==-1)
+            if(j<arr.length)
+                arr[j+1],arr[j]=arr[j],arr[j+1] if ((arr[j]<=>arr[j+1])==-1)
             end
             
         end
     end 
     p arr
 end
-Bubble_sort([-1,-2,4,0,5,-1])
+Bubble_sort([10,-2,4,0,5,1])
 
 def Bubble_sort_by(arr)
     arr.each_with_index do |i,j|
