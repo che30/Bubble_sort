fe509a05157e531476b9c886b04a52cefee5a93f
imran5644 <imi56444@gmail.com>
2020-11-03 22:04:52 +0530

    implemented bubblesort_by

--- main.rb
+++ main.rb
@@ -10,4 +10,19 @@ def Bubble_sort(arr)
     p arr
 end
 Bubble_sort([-1,-2,4,0,5,-1])
-    
+
+def Bubble_sort_by(arr)
+    arr.each_with_index do |i,j|
+        arr.each_with_index do |k,l|
+            if(yield(k,i)>0) 
+                arr[j],arr[l]=arr[l],arr[j]
+            end
+        end
+    end
+    p arr
+end
+Bubble_sort_by(["hi","hello", "hey"]) do |left,right|
+    left.length - right.length
+    end 
+
+
